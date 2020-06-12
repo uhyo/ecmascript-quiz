a6d4d7fa566f9b0cd120fc96ac85ac596f426a8f
uhyo <uhyo@uhy.ooo>
2020-06-13 00:47:27 +0900

    run React

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,11 @@
-console.log("hi")
+import React from "react";
+import ReactDOM, { unstable_createRoot } from "react-dom";
+import { App } from "./App";
+
+const root = document.createElement("div");
+root.id = "root";
+document.body.appendChild(root);
+
+if (root) {
+  unstable_createRoot(root).render(<App />);
+}
